107e9544e10b6408dbc47f330c8af197f6e2af86
cmatth25 <101157734+cmatth25@users.noreply.github.com>
2024-04-16 16:08:31 -0400

    Update decontaminte.sh

--- QC/decontaminte.sh
+++ QC/decontaminte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `ls *.fastq.gz | sed 's/.fastq.gz//g'`;
 do
-	 /usr/local/BBmap/removehuman.sh in=${i}.fastq.gz out=/2/scratch/CraigM/IPP/Metagenomes/MGtotrim/trimmed/clean/${i}_clean.fastq.gz t=18 path=/2/scratch/CraigM/bowtie2ref/
+	 /usr/local/BBmap/removehuman.sh in=${i}.fastq.gz out=/2/scratch/CraigM/IPP/Metagenomes/MGtotrim/trimmed/clean/${i}_clean.fastq.gz t=18 path=/2/scratch/CraigM/bowtie2ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 done
