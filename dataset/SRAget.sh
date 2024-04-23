bebda4206be015f0e41b87e8e2f814bc476f1399
cmatth25 <101157734+cmatth25@users.noreply.github.com>
2024-04-23 00:48:38 -0400

    Update SRAget.sh

--- dataset/SRAget.sh
+++ dataset/SRAget.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+for i in $(cat < "$1");
+do 
+	prefetch $i --max-size 100g
+done
 
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+for i in $(cat < "$1");
+do
+  fasterq-dump $i
+done
+
+gzip *.fastq
